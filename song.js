1a48ab7a609258ebb270bf6b0e77212043693683
Trang Hoang <tychuotsieu45@gmail.com>
2020-03-19 01:58:55 -0400

    x

---
+++ song.js
@@ -0,0 +1,62 @@
+songs = [
+  {
+    "title": "Bartier Cardi",
+    "artists": ["Cardi B", "21 Savage"],
+    "genre": ["Hip Hop", "Rap"],
+    "year_released": "2017"
+  },
+  {
+    "title": "Billionaire",
+    "artists": ["Travie McCoy", "Bruno Mars"],
+    "genre": ["Pop"],
+    "year_released": "2010"
+  },
+  {
+    "title": "Thnks fr th Mmrs",
+    "artists": ["Fall Out Boy"],
+    "genre": ["Pop Punk"],
+    "year_released": "2007"
+  },
+  {
+    "title": "Mi Gente",
+    "artists": ["J Balvin", "Willy William"],
+    "genre": ["Reggaeton"],
+    "year_released": "2017"
+  },
+  {
+    "title": "Hot Girl Summer",
+    "artists": ["Megan Thee Stallion", "Ty Dolla $ign", "Nicki Minaj"],
+    "genre": ["Hip Hop", "Rap"],
+    "year_released": "2019"
+  },
+  {
+    "title": "Taki Taki",
+    "artists": ["DJ Snake, Selena Gomez, Ozuna, Cardi B"],
+    "genre": ["Electronic"],
+    "year_released": "2018"
+  },
+  {
+    
+    "title": "papacito",
+    "artists": ["Alaina Castillo"],
+    "genre": ["Pop"],
+    "year_released": "2019"
+  },
+  {
+    "title": "CRZY",
+    "artists": ["Kehlani"],
+    "genre": ["R&B"],
+    "year_released": "2016"
+  },
+  {
+    "title": "If the World Was Ending","artists": ["JP Saxe, Julia Michaels"],
+    "genre": ["Pop"],
+    "year_released": "2019"
+  },
+  {
+    "title": "Hood Go Crazy",
+    "artists": ["Tech N9ne, 2 Chainz, B.o.B"],
+    "genre": ["Hip Hop", "Rap"],
+    "year_released": "2015"
+  }
+]
